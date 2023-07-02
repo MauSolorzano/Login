6020afb9640e812267c6f33d017bdc3a013acc25
Gerson-Solano <83689574+Gerson-Solano@users.noreply.github.com>
2023-07-02 00:54:43 -0600

    cambios

--- config/config.php
+++ config/config.php
@@ -13,6 +13,6 @@ db = 'u484426513_apireact'#nombre de la base datos -->
     define('CHARSET', 'utf8mb4');
     
     //Url del proyecto web
-    define('URL', 'http://localhost/Login/');
+    define('URL', 'http://localhost/UCR-2023/Login/');
 
 ?>
--- model/usuariosmodel.php
+++ model/usuariosmodel.php
@@ -3,15 +3,18 @@
 include_once 'class/usuarios.php';
 
 
-class UsuariosModel extends Model{
+class UsuariosModel extends Model
+{
 
-    
 
-    public function __construct(){
+
+    public function __construct()
+    {
         parent::__construct();
     }
 
-    public function getUsuarios(){        
+    public function getUsuarios()
+    {
         $items = [];
 
         try {
@@ -19,7 +22,7 @@ class UsuariosModel extends Model{
             $stringSQL = "SELECT * FROM `user` order by id DESC;";
             $query = $this->db->connect()->query($stringSQL);
 
-            while ( $row = $query->fetch()){//obtiene todas las filas
+            while ($row = $query->fetch()) { //obtiene todas las filas
                 $item = new classUsuarios();
 
                 foreach ($row as $key => $value) {
@@ -35,15 +38,18 @@ class UsuariosModel extends Model{
         }
     }
 
-    public function insertarUsuario($datos){
+    public function insertarUsuario($datos)
+    {
         try {
             //code...
             $datos['id'] = "0";
+            $password = $datos['password'];
+            $SSLPassword = openssl_encrypt($password, 'AES-256-CBC', 'clave_secreta', 0, 'vector_inicializacion');
+            $datos['password'] = $SSLPassword;
             $stringSQL = 'INSERT INTO user(id, name, email, password) VALUES ( :id, :name, :email, :password);';
             $query = $this->db->connect()->prepare($stringSQL);
             $query->execute($datos);
             return true;
-
         } catch (PDOException $th) {
             //throw $th;
             //var_dump($th);
@@ -51,87 +57,126 @@ class UsuariosModel extends Model{
         }
     }
 
-    public function verUsuarios($id){
+    public function verUsuarios($id)
+    {
         try {
             $item = new classUsuarios();
             //code...
             $stringSQL = "Select * FROM `user` where id=:id;";
             $query = $this->db->connect()->prepare($stringSQL);
-            $query->execute(['id'=>$id]);
+            $query->execute(['id' => $id]);
 
-            while ( $row = $query->fetch()){//obtiene la fila
+            while ($row = $query->fetch()) { //obtiene la fila
                 foreach ($row as $key => $value) {
                     # code...
                     $item->$key = $value;
-                   // $_SESSION['autenticado'] = true;
+                    // $_SESSION['autenticado'] = true;
                 }
             }
             return $item;
         } catch (PDOException $th) {
             //throw $th;
             return [];
-        }           
+        }
     }
-      //actualizarcurso
-      public function actualizarUsuario($datos){
+    //actualizarcurso
+    public function actualizarUsuario($datos)
+    {
         try {
-          
+
             $stringSQL = 'UPDATE user SET name=:name,email=:email,password=:password WHERE id=:id ;';
             $query = $this->db->connect()->prepare($stringSQL);
             $query->execute($datos);
             return true;
-
         } catch (PDOException $th) {
             //throw $th;
             var_dump($th);
             return false;
         }
-    }   
+    }
 
     //eliminarcurso
-    public function eliminarUsuario($id){        
-        try {            
+    public function eliminarUsuario($id)
+    {
+        try {
             //code...
             $stringSQL = "DELETE FROM `user` WHERE id =:id;";
             $query = $this->db->connect()->prepare($stringSQL);
-            $query->execute(['id'=>$id]);
-            
+            $query->execute(['id' => $id]);
+
             return true;
         } catch (PDOException $th) {
             //throw $th;
             return false;
-        }           
+        }
     }
 
-    function validarCredenciales($email, $password){
-    
+    function validarCredenciales($email, $password)
+    {
+
         try {
-            //code...
-            $stringSQL = "Select * FROM `user` where email=:email and password = :password;";
+            $stringSQL = "SELECT * FROM `user` WHERE email = :email;";
             $query = $this->db->connect()->prepare($stringSQL);
-            $query->execute(['email'=>$email, 'password'=>$password]);
+            $query->execute(['email' => $email]);
     
             $rowCount = $query->rowCount();
-
-            
+    
             if ($rowCount > 0) {
-
-                $_SESSION['usuario'] = $email;
-        
-                // La consulta fue exitosa
-                return true;
-
+                $row = $query->fetch();
+                $encryptedPassword = $row['password'];
+                $decryptedPassword = openssl_decrypt($encryptedPassword, 'AES-256-CBC', 'clave_secreta', 0, 'vector_inicializacion');
+    
+                if ($password === $decryptedPassword) {
+                    $_SESSION['usuario'] = $email;
+                    return true;
+                } else {
+                    return false;
+                }
             } else {
-                // No se encontraron registros que coincidan
                 return false;
             }
-           
         } catch (PDOException $th) {
-            //throw $th;
-            return $th;
-        }           
+            return false;
+        }
     }
-
 }
 
 ?>
+
+/** function validarCredenciales($email, $password){
+
+try {
+//code...
+$OpenPassword = openssl_decrypt($password, 'AES-256-CBC', 'clave_secreta', 0, 'vector_inicializacion');
+var_dump($OpenPassword);
+if ($password == $OpenPassword) {
+$password = $OpenPassword;
+var_dump($OpenPassword);
+$stringSQL = "Select * FROM `user` where email=:email and password = :password;";
+$query = $this->db->connect()->prepare($stringSQL);
+$query->execute(['email'=>$email, 'password'=>$password]);
+}else {
+$stringSQL = "Select * FROM `user` where email=:email and password = :password;";
+$query = $this->db->connect()->prepare($stringSQL);
+$query->execute(['email'=>$email, 'password'=>$password]);
+}
+$rowCount = $query->rowCount();
+
+
+if ($rowCount > 0) {
+
+$_SESSION['usuario'] = $email;
+
+// La consulta fue exitosa
+return true;
+
+} else {
+// No se encontraron registros que coincidan
+return false;
+}
+
+} catch (PDOException $th) {
+//throw $th;
+return $th;
+}
+} */
